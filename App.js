e27be91c3ecacf90adf505260e067ea4c3b65a52
whitehatjr <rajeev@whitehatjr.com>
2020-01-19 12:36:40 +0530

    Add Tab navigation

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import TransactionScreen from './TransactionScreen';
+import SearchScreen from './SearchScreen';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <AppContainer />
+      
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Transaction: {screen: TransactionScreen},
+  Search: {screen: SearchScreen},
+});
+
+const AppContainer =  createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
