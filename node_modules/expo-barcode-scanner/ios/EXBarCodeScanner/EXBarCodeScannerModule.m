1963324c1ec953f1f52c9d134a51486bb37b070b
vishalgaddam873 <vishal18@navgurukul.org>
2021-01-27 12:33:40 +0530

    Version updated

---
+++ node_modules/expo-barcode-scanner/ios/EXBarCodeScanner/EXBarCodeScannerModule.m
@@ -0,0 +1,96 @@
+// Copyright 2016-present 650 Industries. All rights reserved.
+
+#import <EXBarCodeScanner/EXBarCodeScannerModule.h>
+#import <EXBarCodeScanner/EXBarCodeScannerUtils.h>
+#import <EXBarCodeScanner/EXBarCodeCameraRequester.h>
+#import <UMImageLoaderInterface/UMImageLoaderInterface.h>
+#import <UMPermissionsInterface/UMPermissionsInterface.h>
+#import <UMPermissionsInterface/UMPermissionsMethodsDelegate.h>
+
+@interface EXBarCodeScannerModule ()
+
+@property (nonatomic, weak) UMModuleRegistry *moduleRegistry;
+@property (nonatomic, weak) id<UMImageLoaderInterface> imageLoader;
+@property (nonatomic, weak) id<UMPermissionsInterface> permissionsManager;
+
+@end
+
+@implementation EXBarCodeScannerModule
+
+UM_EXPORT_MODULE(ExpoBarCodeScannerModule);
+
+- (void)setModuleRegistry:(UMModuleRegistry *)moduleRegistry
+{
+  _moduleRegistry = moduleRegistry;
+  _imageLoader = [moduleRegistry getModuleImplementingProtocol:@protocol(UMImageLoaderInterface)];
+  _permissionsManager = [moduleRegistry getModuleImplementingProtocol:@protocol(UMPermissionsInterface)];
+  [UMPermissionsMethodsDelegate registerRequesters:@[[EXBareCodeCameraRequester new]] withPermissionsManager:_permissionsManager];
+}
+
+- (NSDictionary *)constantsToExport
+{
+  return @{
+           @"Type": @{
+               @"front": @(EXCameraTypeFront),
+               @"back" : @(EXCameraTypeBack),
+               },
+           @"BarCodeType": [EXBarCodeScannerUtils validBarCodeTypes],
+           };
+}
+
+UM_EXPORT_METHOD_AS(getPermissionsAsync,
+                    getPermissionsAsync:(UMPromiseResolveBlock)resolve
+                    rejecter:(UMPromiseRejectBlock)reject)
+{
+  [UMPermissionsMethodsDelegate getPermissionWithPermissionsManager:_permissionsManager
+                                                      withRequester:[EXBareCodeCameraRequester class]
+                                                            resolve:resolve
+                                                             reject:reject];
+}
+
+UM_EXPORT_METHOD_AS(requestPermissionsAsync,
+                    requestPermissionsAsync:(UMPromiseResolveBlock)resolve
+                    rejecter:(UMPromiseRejectBlock)reject)
+{
+  [UMPermissionsMethodsDelegate askForPermissionWithPermissionsManager:_permissionsManager
+                                                         withRequester:[EXBareCodeCameraRequester class]
+                                                               resolve:resolve
+                                                                reject:reject];
+}
+
+UM_EXPORT_METHOD_AS(scanFromURLAsync,
+                    scanFromURLAsync:(NSString *)url
+                    barCodeTypes:(NSArray *)barCodeTypes
+                    resolver:(UMPromiseResolveBlock)resolve
+                    rejecter:(UMPromiseRejectBlock)reject)
+{
+  // We only support QR codes, so barCodeTypes is ignored
+  NSURL *imageURL = [NSURL URLWithString:url];
+  [_imageLoader loadImageForURL:imageURL completionHandler:^(NSError *error, UIImage *loadedImage) {
+    if (error != nil) {
+      reject(@"E_IMAGE_RETRIEVAL_ERROR", @"Could not get the image", error);
+      return;
+    }
+    
+    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeQRCode
+                                              context:nil
+                                              options:@{CIDetectorAccuracy:CIDetectorAccuracyHigh}];
+    if (detector)  {
+      NSArray *features = [detector featuresInImage:[[CIImage alloc] initWithCGImage:loadedImage.CGImage]];
+      
+      NSMutableArray *result = [NSMutableArray arrayWithCapacity:1];
+      for (CIQRCodeFeature *feature in features)  {
+        [result addObject:@{
+                            @"type" : AVMetadataObjectTypeQRCode,
+                            @"data" : feature.messageString
+                            }];
+      }
+      
+      resolve(result);
+    } else {
+      reject(@"E_SCANNER_INIT_FAILED", @"Could not initialize the barcode scanner", nil);
+    }
+  }];
+}
+
+@end
---
+++ node_modules/expo-barcode-scanner/ios/EXBarCodeScanner/EXBarCodeScannerViewManager.h
@@ -0,0 +1,9 @@
+// Copyright 2016-present 650 Industries. All rights reserved.
+
+#import <AVFoundation/AVFoundation.h>
+#import <UMCore/UMViewManager.h>
+#import <UMCore/UMModuleRegistryConsumer.h>
+
+@interface EXBarCodeScannerViewManager : UMViewManager <UMModuleRegistryConsumer>
+
+@end
